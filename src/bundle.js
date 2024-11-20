6c988eb040d87c6ae2cb3c0e05d3c203b79dd65b
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-11-20 14:51:39 +0800

    feat(wasm.lua): pass options to Luacheck

--- src/bundle.js
+++ src/bundle.js
@@ -4,5 +4,8 @@ const fs = require('fs');
 const {bundle} = require('luabundle');
 
 const bundledLua = bundle('./luacheck/init.lua', {force: true, isolate: true, metadata: false}),
-	i = bundledLua.lastIndexOf('\n');
-fs.writeFileSync('bundle.json', JSON.stringify(`${bundledLua.slice(0, i + 1)}luacheck =${bundledLua.slice(i + 7)}`));
+	i = bundledLua.lastIndexOf('\n'),
+	f = fs.readFileSync('wasm.lua', 'utf8'),
+	s = `${bundledLua.slice(0, i + 1)}local luacheck=${bundledLua.slice(i + 7)}\n${f}`;
+fs.writeFileSync('bundle.lua', `${s}\nreturn check`);
+fs.writeFileSync('bundle.json', JSON.stringify(s));

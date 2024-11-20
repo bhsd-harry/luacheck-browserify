354049f40b85e5f7b0e52304d7bedf0c93395ded
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-11-20 13:30:41 +0800

    fix(package.json): devDependencies

--- src/wasm.js
+++ src/wasm.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const {LuaFactory} = require('wasmoon');
-const script = require('../src/bundle.json');
+const script = require('./bundle.json');
 
 (async () => {
 	const factory = new LuaFactory(),
 		lua = await factory.createEngine();
 	await lua.doString(script);
-	Object.assign(globalThis, {luacheck: lua.global.get('luacheck')});
+	Object.assign(globalThis, {luacheck: lua.global.get('check')});
 })();

076e44a1bf8a1e0e1e4d293808955e1959a953fc
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-28 17:26:24 +0800

    refactor(test.js): reduce duplication

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ or
 After Luacheck-browserify is installed, a global async function `luacheck` is available. The `luacheck` function takes a required string argument to specify the standard globals similar to the [`--std` CLI option](https://luacheck.readthedocs.io/en/stable/cli.html#command-line-options), and its return value resolves with a class instance with an async `queue` method that can be called with a string of Lua code to check. The `queue` method returns a promise that resolves with an array of warnings.
 
 ```javascript
-const Luacheck = await luacheck('max');
+const Luacheck = luacheck('max');
 console.log(await Luacheck.queue('local a, b, c = nil'));
 ```
 
--- spec/test.js
+++ spec/test.js
@@ -1,39 +1,43 @@
 /* global luacheck */
 'use strict';
 
+/** @typedef {import('../dist/wasm').Diagnostic} Diagnostic */
+
 const fs = require('fs'),
 	path = require('path'),
 	assert = require('assert'),
-	/** @type {Record<string, import('../dist/wasm').Diagnostic[]>} */ tests = require('./tests.json');
+	/** @type {Record<string, Diagnostic[]>} */ tests = require('./tests.json');
 
-describe('Luacheck', () => {
+/**
+ * 测试
+ * @param {(code: string, file: string) => Promise<Diagnostic[]>} diagnose 诊断函数
+ */
+const execute = diagnose => {
 	for (const file of fs.readdirSync('spec/samples/')) {
 		if (file.endsWith('.lua')) {
 			it(file, async () => {
 				const code = fs.readFileSync(path.join('spec', 'samples', file), 'utf8'),
-					warnings = await (await luacheck()).queue(code),
-					oldWarnings = tests[file];
-				tests[file] = warnings.map(
-					warning => Object.fromEntries(Object.entries(warning).sort(([a], [b]) => a.localeCompare(b))),
-				);
+					oldWarnings = tests[file],
+					warnings = await diagnose(code, file);
 				assert.deepStrictEqual(warnings, oldWarnings);
 			});
 		}
 	}
+};
+
+describe('Luacheck', () => {
+	execute(async (code, file) => {
+		const warnings = await luacheck().queue(code);
+		tests[file] = warnings.map(
+			warning => Object.fromEntries(Object.entries(warning).sort(([a], [b]) => a.localeCompare(b))),
+		);
+		return warnings;
+	});
 	after(() => {
 		fs.writeFileSync('spec/tests.json', `${JSON.stringify(tests, null, '\t')}\n`);
 	});
 });
 
 describe('luacheck.check', () => {
-	for (const file of fs.readdirSync('spec/samples/')) {
-		if (file.endsWith('.lua')) {
-			it(file, async () => {
-				const code = fs.readFileSync(path.join('spec', 'samples', file), 'utf8'),
-					warnings = await luacheck.check(code),
-					oldWarnings = tests[file];
-				assert.deepStrictEqual(warnings, oldWarnings);
-			});
-		}
-	}
+	execute(code => luacheck.check(code));
 });

fcbb34186fcc45bc1a66f0bb3c60308679b894b9
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-05-09 03:15:46 +0800

    chore(package.json): upgrade

--- .eslintrc.dist.cjs
+++ .eslintrc.dist.cjs
@@ -7,8 +7,6 @@ module.exports = {
 	...config,
 	rules: {
 		...config.rules,
-		'es-x/no-bigint': 0,
-		'es-x/no-dynamic-import': 0,
 		'es-x/no-resizable-and-growable-arraybuffers': 0,
 		'es-x/no-weakrefs': 0,
 	},
